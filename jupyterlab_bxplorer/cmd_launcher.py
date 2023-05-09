edd10132296ecb842da1b24d1f79959086ff5026
JupyterLab <frivas@navteca.com>
2023-05-09 11:53:08 +0000

    Removing unnecessary functions

--- jupyterlab_bxplorer/cmd_launcher.py
+++ jupyterlab_bxplorer/cmd_launcher.py
@@ -60,16 +60,6 @@ class ApplicationNotFound(Error):
         return f"The specified application has not been found."
 
 
-def convertSize(size):
-    if (size == 0):
-        return '0B'
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB","ZB", "YB")
-    i = int(math.floor(math.log(size,1024)))
-    p = math.pow(1024,i)
-    s = round(size/p,2)
-    return '%.2f %s' % (s,size_name[i])
-
-
 def get_size(bucket: str, path: str, client):
     my_bucket = client.Bucket(bucket)
     total_size = 0
