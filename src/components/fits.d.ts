8f75301d84d43dfe0a4d2133c2df3c8730da6c95
Luis Leon <lleon@navteca.com>
2025-06-09 14:29:34 -0600

    Cleaning before adding all new version 2 structure

--- src/components/fits.d.ts
+++
@@ -1,3 +0,0 @@
-export type FitsContextType = {
-    getFitsHeader: (file: string, bucket: string, anon: boolean) => Promise<string>;
-};
--- src/svg.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.svg' {
-  const value: string;
-  export default value;
-}
-
-declare module 'path';
